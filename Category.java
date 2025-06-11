e3d74b4281542e4683d4991d6e3e4c5b9bc7d349
Thoposaurus <149897944+khaleelclark@users.noreply.github.com>
2025-06-10 21:35:39 -0400

    Created enum class

--- Category.java
+++ Category.java
@@ -1,3 +1,25 @@
 public enum Category {
+    FOOD_BEVERAGES("Food & Beverages"),
+    PERSONAL_CARE("Personal Care & Toiletries"),
+    ELECTRONICS("Electronics"),
+    HOUSEHOLD_ITEMS("Household Items"),
+    CLEANING_SUPPLIES("Cleaning Supplies"),
+    OFFICE_AND_SCHOOL("Office and School Supplies"),
+    CLOTHING("Clothing & Accessories"),
+    TOOLS_HARDWARE("Tools & Hardware"),
+    HEALTH_WELLNESS("Health & Wellness"),
+    ENTERTAINMENT("Entertainment & Media"),
+    OUTDOOR_AND_SPORTS("Outdoor & Sports"),
+    PET_SUPPLIES("Pet Supplies"),
+    MISC("Miscellaneous");
 
+    private final String categoryName;
+
+    Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
 }
