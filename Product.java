e11728a31e26c8ca66d4cd4655994cff92e3cb0c
Thoposaurus <149897944+khaleelclark@users.noreply.github.com>
2025-06-10 21:47:16 -0400

    Created Product class

--- Product.java
+++ Product.java
@@ -1,2 +1,46 @@
 public class Product {
+    private String name;
+    private int quantity;
+    private int expectedQuantity;
+    private Category category;
+    private String location;
+
+    public Product(String name, int quantity, int expectedQuantity, Category category, String location) {
+        this.name = name;
+        this.quantity = quantity;
+        this.expectedQuantity = expectedQuantity;
+        this.category = category;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public int getExpectedQuantity() {
+        return expectedQuantity;
+    }
+    public void setExpectedQuantity(int expectedQuantity) {
+        this.expectedQuantity = expectedQuantity;
+    }
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- ProductList.java
+++ ProductList.java
@@ -1,2 +1,12 @@
-public class ProductList {
+import java.util.ArrayList;
+
+public class ProductList extends ArrayList<Product> {
+    public double getTotalInventoryEstimate(){
+        //TODO
+        return 0;
+    }
+    public ArrayList<Product> getUnderstockedProducts(){
+        return null;
+        //TODO
+    }
 }
