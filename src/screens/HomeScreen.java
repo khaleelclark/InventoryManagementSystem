d1bab2e12c981bb5becf4d14ddf33a8d9642f01a
Thoposaurus <149897944+khaleelclark@users.noreply.github.com>
2025-07-05 00:23:39 -0400

    Centered buttons & added title

--- src/screens/HomeScreen.java
+++ src/screens/HomeScreen.java
@@ -1,17 +1,26 @@
 package src.screens;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import src.ProductManager;
 import src.ScreenController;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 
 public class HomeScreen {
-    private final VBox layout;
+    private final GridPane layout;
 
     public HomeScreen(ScreenController controller) {
-        layout = new VBox(10);
+        layout = new GridPane();
+
+        layout.setMinSize(500, 250);
+        layout.setPadding(new Insets(20));
+        layout.setVgap(15);
+        layout.setHgap(15);
+        layout.setAlignment(Pos.CENTER);
 
         Button addButton = new Button("Add Product");
         Button viewButton = new Button("View Products");
@@ -25,20 +34,35 @@ public class HomeScreen {
         viewButton.setOnAction(_ -> controller.activate("view"));
         updateButton.setOnAction(_ -> controller.activate("update"));
         removeButton.setOnAction(_ -> controller.activate("remove"));
-        understockedProductsButton.setOnAction(_ -> controller.activate("understocked"));
         updateQuantityButton.setOnAction(_ -> controller.activate("quantity"));
+        understockedProductsButton.setOnAction(_ -> controller.activate("understocked"));
         calculateInvEstimateButton.setOnAction(_ -> {
-
             double totalEstimate = ProductManager.getProducts().getTotalInventoryEstimate();
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Inventory Estimate");
             alert.setHeaderText("Total Inventory Value");
             alert.setContentText("Your estimated inventory value is: $" + String.format("%.2f", totalEstimate));
-
             alert.showAndWait();
         });
 
-        layout.getChildren().addAll(addButton, viewButton, updateButton, removeButton, updateQuantityButton, understockedProductsButton, calculateInvEstimateButton);
+        Label title = new Label("Welcome to Zindel's Inventory Management System");
+        title.setStyle("-fx-font-weight: bold; -fx-font-size: 16px;");
+
+        Label description = new Label("Please select from one of the following options below:");
+        description.setStyle("-fx-font-weight: bold;");
+        description.setPadding(new Insets(10, 10, 10, 10));
+
+        layout.add(title, 0, 0, 2, 1);
+        layout.add(description, 0, 1, 2, 1);
+
+        layout.add(addButton, 0, 2);
+        layout.add(viewButton, 1, 2);
+        layout.add(updateButton, 0, 3);
+        layout.add(removeButton, 1, 3);
+        layout.add(updateQuantityButton, 0, 4);
+        layout.add(understockedProductsButton, 1, 4);
+        layout.add(calculateInvEstimateButton, 0, 5, 2, 1); // center-aligned, spans 2 columns
+
     }
 
     public Parent getView() {
