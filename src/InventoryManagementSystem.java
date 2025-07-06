caa37e083468b523a1e14adac12b08731e97745b
Thoposaurus <149897944+khaleelclark@users.noreply.github.com>
2025-07-05 21:59:37 -0400

    Refactored main class

--- src/InventoryManagementSystem.java
+++ src/InventoryManagementSystem.java
@@ -3,7 +3,7 @@ package src;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class InventoryManagementSystem extends Application {
 
     @Override
     public void start(Stage primaryStage) {
