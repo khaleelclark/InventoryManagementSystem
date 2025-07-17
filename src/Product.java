e9ad03ceee31e7f523bf617442708da20adae47d
Khaleel Clark <149897944+khaleelclark@users.noreply.github.com>
2025-07-17 13:16:36 -0400

    Created JavaDoc comments for the Product class

--- src/Product.java
+++ src/Product.java
@@ -1,14 +1,15 @@
 package src;
 
 /**
- * Khaleel Zindel Clark
- * CEN 3024 - Software Development 1
- * June 18, 2025
- * Product.java
- * This class creates a product object with the attributes
- * name, quantity, expected quantity, estimated cost, category, and location
+ * Represents a product in the inventory with attributes such as
+ * name, quantity, expected quantity, estimated cost, category, and location.
+ *
+ * <p>This class provides getters and setters for all attributes,
+ * along with methods to retrieve formatted product information.</p>
+ *
+ * @author Khaleel Zindel Clark
+ * @version July 18, 2025
  */
-
 public class Product {
     private String name;
     private int quantity;
@@ -17,6 +18,17 @@ public class Product {
     private Category category;
     private String location;
 
+
+    /**
+     * Constructs a new {@code Product} with the specified attributes.
+     *
+     * @param name            the product name
+     * @param quantity        the current quantity
+     * @param expectedQuantity the expected quantity to maintain
+     * @param estimatedCost   the estimated cost of the product
+     * @param category        the product category
+     * @param location        the storage location of the product
+     */
     public Product(String name, int quantity, int expectedQuantity, double estimatedCost, Category category, String location) {
         this.name = name;
         this.quantity = quantity;
@@ -27,142 +39,74 @@ public class Product {
     }
 
     /**
-     * method: getName
-     * parameters: none
-     * return: String
-     * purpose: this method is a getter to retrieve the name of a product
+     * Returns the product name.
+     * @return the product name
      */
     public String getName() {
         return name;
     }
 
     /**
-     * method: setName
-     * parameters: String name
-     * return: void
-     * purpose: this method is a setter to set the name of a product
+     * Sets the product name.
+     * @param name the product name to set
      */
     public void setName(String name) {
         this.name = name;
     }
 
     /**
-     * method: getQuantity
-     * parameters: none
-     * return: int
-     * purpose: this method is a getter to retrieve the quantity of a product
+     * Returns the product quantity.
+     * @return the product quantity
      */
     public int getQuantity() {
         return quantity;
     }
 
     /**
-     * method: setQuantity
-     * parameters: int Quantity
-     * return: void
-     * purpose: this method is a setter to set the quantity of a product
+     * Sets the product quantity.
+     * @param quantity the product quantity to set
      */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
     /**
-     * method: getExpectedQuantity
-     * parameters: none
-     * return: int
-     * purpose: this method is a getter to retrieve the expected quantity of a product
+     * Returns the product expected quantity.
+     * @return the product expected quantity
      */
     public int getExpectedQuantity() {
         return expectedQuantity;
     }
 
     /**
-     * method: setExpectedQuantity
-     * parameters: int expectedQuantity
-     * return: void
-     * purpose: this method is a setter to set the expected quantity of a product
-     */
-    public void setExpectedQuantity(int expectedQuantity) {
-        this.expectedQuantity = expectedQuantity;
-    }
-
-    /**
-     * method: getEstimatedCost
-     * parameters: none
-     * return: double
-     * purpose: this method is a getter to retrieve the estimated cost of a product
+     * Returns the product estimated cost.
+     * @return the product estimated cost
      */
     public double getEstimatedCost() {
         return estimatedCost;
     }
 
     /**
-     * method: setEstimatedCost
-     * parameters: double estimatedCost
-     * return: void
-     * purpose: this method is a setter to set the estimated cost of a product
-     */
-    public void setEstimatedCost(double estimatedCost) {
-        this.estimatedCost = estimatedCost;
-    }
-
-    /**
-     * method: getCategory
-     * parameters: none
-     * return: Category
-     * purpose: this method is a getter to retrieve the Category of a product
+     * Returns the product category.
+     * @return the product category
      */
     public Category getCategory() {
         return category;
     }
 
     /**
-     * method: setLocation
-     * parameters: Category category
-     * return: void
-     * purpose: this method is a setter to set the category of a product
+     * Sets the product category.
+     * @param category the product category to set
      */
     public void setCategory(Category category) {
         this.category = category;
     }
 
     /**
-     * method: getLocation
-     * parameters: none
-     * return: String
-     * purpose: this method is a getter to retrieve the location of a product
+     * Returns the product location.
+     * @return the product location
      */
     public String getLocation() {
         return location;
-    }
-
-    /**
-     * method: setLocation
-     * parameters: String location
-     * return: void
-     * purpose: this method is a setter to set the location of a product
-     */
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    /**
-     * method: getPatronInformation
-     * parameters: none
-     * return: String
-     * purpose: this method returns patron attribute information in a formatted string
-     */
-    public String getProductInformation() {
-        return "Name: " + name + " Quantity: " + quantity + " Expected Quantity: " + expectedQuantity + " Estimated Cost: " + estimatedCost + " Category: " + category.getCategoryName() + " Location: " + location;
-    }
-
-    /**
-     * method: getPatronInformation
-     * parameters: none
-     * return: String
-     * purpose: this method returns patron attribute information in a formatted string
-     */
-    public String getQuantityInformation() {
-        return "Name: " + name + " Quantity: " + quantity + " Expected Quantity: " + expectedQuantity;
     }
 }
